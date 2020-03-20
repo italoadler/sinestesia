14dfeb5cda71913684408cd94415046df0490dc5
italoadler <adler.italo@gmail.com>
2020-03-20 18:57:32 -0300

    Asset atualizado

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ MVP 0.0.1
 
 Imagem sendo afetado pelo audio, visualizador básico. 
 
+* Visualizador linear 
+* Visualizador radial 
+* Mapeamento  
+* Captar entrada de áudio 
--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+function preload(){
+  sound = loadSound('assets/beat.mp3');
+}
+
 function setup() {
-  // put setup code here
+  let cnv = createCanvas(displayWidth, displayHeight);
+  cnv.mouseClicked(toggleSound);
+  amplitude = new p5.Amplitude();
 }
 
 function draw() {
-  // put drawing code here
+  background(220, 150);
+  textAlign(CENTER);
+  text('Clique para tocar', width/2, 20);
+
+  let level = amplitude.getLevel();
+  let size = map(level, 0, 1, 0, 200);
+  ellipse(width/2, height/2, size, size);
 }
+
+function toggleSound(){
+  if (sound.isPlaying()) {
+    sound.stop();
+  } else {
+    sound.play();
+  }
+}}
